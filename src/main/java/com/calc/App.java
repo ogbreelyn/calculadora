a57e379fa3d46fa3f31abd637e7619b7ebf45fc1
birabor <brelyniraborramos@gmail.com>
2025-04-08 13:37:50 +0200

    subida inicial

---
+++ src/main/java/com/calc/App.java
@@ -0,0 +1,19 @@
+package com.calc;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+       
+    }
+    
+}
